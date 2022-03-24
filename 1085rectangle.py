eecc4afa8cd09a529bc85e35b7691dd4358d0e51
kws10 <82077024+kws10@users.noreply.github.com>
2022-03-24 17:18:03 +0900

    Create 1085rectangle.py

---
+++ 1085rectangle.py
@@ -0,0 +1,16 @@
+x,y,w,h = map(int,input().split())
+
+width = w-x
+height = h-y
+
+if width<height:
+   if width>x:
+      print(x)
+   else:
+      print(width)
+
+else:
+   if height>y:
+      print(y)
+   else:
+      print(height)
