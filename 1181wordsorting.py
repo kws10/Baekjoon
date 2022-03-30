6c1fc2e3388af08af0f603d249cf2356dd4d7dab
kws10 <82077024+kws10@users.noreply.github.com>
2022-03-30 13:06:40 +0900

    .

--- 1181wordsorting.py
+++ 1181wordsorting.py
@@ -1,12 +1,14 @@
-cnt = int(input())
-word_list=[]
+cnt = int(input()) # 단어 개수 입력받고
+word_list=[] # 단어리스트 선언하고
 
-for i in range(cnt):
-    word_list.append(input())
+for i in range(cnt): #단어 개수 만큼 반복문 돌리고
+    word_list.append(input()) # 리스트에 단어 입력받고
 
-word_list.sort()
-word_list.sort(key=len)
+set_list = set(word_list) #리스트에 중복을 제거
+word_list = list(set_list) #다시 리스트로 변환
+word_list.sort() #글자를 사전순서로 정렬하고
+word_list.sort(key=len) #글자순서로 정렬
 
-for j in word_list:
-    print(j)
+for j in word_list: #워드리스트 돌면서
+    print(j) # 출력
 
---
+++ 1259palindrome.py
@@ -0,0 +1,19 @@
+while 1:
+    a = input()
+
+    if a=='0':
+       break
+
+    if a==a[::-1]: # 배열이 배열의 역순을취한것과 같으면
+       print('yes')
+    else:
+       print('no')
+
+
+
+
+
+
+
+
+
