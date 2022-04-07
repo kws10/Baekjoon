519c2e6d3ed1229f223336c65c17d408d6688bb9
kws10 <82077024+kws10@users.noreply.github.com>
2022-04-07 09:43:50 +0900

    Create 1920findnum.py

---
+++ 1920findnum.py
@@ -0,0 +1,15 @@
+
+a = int(input())
+list_a = []
+list_b = []
+answer = []
+
+for i in range(a):
+    list_a.append(input())
+
+b = int(input())
+
+for i in range(b):
+    list_b.append(input())
+
+
