3d4b0efe47128cc890712595a21e57cc10031ff2
kws10 <82077024+kws10@users.noreply.github.com>
2022-04-09 07:32:33 +0900

    Update 1920findnum.py

--- 1920findnum.py
+++ 1920findnum.py
@@ -1,9 +1,24 @@
 # 이진 탐색으로 풀어야됨
 # 이진 탐색은 정렬 되어있는 리스트에서 적용
 
-def binary_search(a,x): # (리스트 , 서치값 )
+def binary_search(a,search): # (리스트 , 서치값 )
     start = 0
     end = len(a) - 1
+
+    mid = (start + end) // 2
+
+    if mid == search:
+       return mid
+    elif mid < search:
+       start = mid
+    elif mid > search:
+       end = mid
+
+    
+
+
+
+
 
 
 
