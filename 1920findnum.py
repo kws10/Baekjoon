bf4035a98b2fac0cdea5bb7d6480730b2f8de92e
kws10 <82077024+kws10@users.noreply.github.com>
2022-04-14 00:21:27 +0900

    .

--- 1920findnum.py
+++ 1920findnum.py
@@ -1,22 +1,41 @@
 # 이진 탐색으로 풀어야됨
 # 이진 탐색은 정렬 되어있는 리스트에서 적용
 
-def binary_search(a,search): # (리스트 , 서치값 )
-    start = 0
-    end = len(a) - 1
+import sys
 
-    mid = (start + end) // 2
+list_n = []
+find = []
 
-    if mid == search:
-       return mid
-    elif mid < search:
-       start = mid
-    elif mid > search:
-       end = mid
+def binary_search(arr,search,start,end): # (리스트 , 서치값, start , end)
+
+    while start <= end:
+          mid = start + end // 2
+
+          if arr[mid] == search:
+             return True
+          elif arr[mid] < search:
+             start = mid+1
+          else:
+             end = mid-1
+
+    return False
+
+n = int(input())
+list_n = list(map(int, sys.stdin.readline().split()))
+
+k = int(input())
+find = list(map(int, sys.stdin.readline().split()))
+
+list_n.sort()
+
+for i in range(k):
+    if binary_search(list_n,find[i],0,len(list_n)-1):
+       print("1")
+    else:
+       print("0")
 
 
-for i in range 
-    
+
 
 
 
