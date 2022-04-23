f3c9c3c439169f45b1f3da0d4f73567683b392b2
kws10 <82077024+kws10@users.noreply.github.com>
2022-04-23 16:15:39 +0900

    .

--- 4153righttriangle.py
+++ 4153righttriangle.py
@@ -7,7 +7,6 @@ while True:
     else:
        if((x**2 == y**2 + z**2) or (y**2 == x**2 + z**2) or (z**2 == x**2 + y**2)):
           print('right')
-
        else:
            
           print('wrong')
