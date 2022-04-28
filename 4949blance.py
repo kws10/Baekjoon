48d49fee6d9e55c4003717e80f564110c4ebea85
kws10 <82077024+kws10@users.noreply.github.com>
2022-04-29 00:44:01 +0900

    Update 4949blance.py

--- 4949blance.py
+++ 4949blance.py
@@ -1,3 +1,21 @@
 
-bl_check=0
+import sys
+
+snt = list(sys.stdin.readline().split())
+
+def check(n):
+    if(n.count('(') == n.count(')') and n.count('[') == n.count(']')):
+       return True
+    else:
+       return False
+
+while(snt != '.'):
+    if check(snt)==True:
+       print('yes')
+    else:
+       print('no')
+
+    snt = list(sys.stdin.readline().split())
+
+
 
